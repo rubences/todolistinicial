a5117b787c62d15632513aab8618663a3170332a
rubences <rjuarcad@uax.es>
2024-10-28 18:48:27 +0100

    Update README.md

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
 /* Populate tables */
 INSERT INTO usuarios (id, email, nombre, password, fecha_nacimiento) VALUES('1', 'user@ua', 'Usuario Ejemplo', '123', '2001-02-10');
+INSERT INTO usuarios (id, email, nombre, password, fecha_nacimiento) VALUES('2', 'admin@ua', 'Admin Ejemplo', '123', '2001-02-10');
 INSERT INTO tareas (id, titulo, usuario_id) VALUES('1', 'Lavar coche', '1');
 INSERT INTO tareas (id, titulo, usuario_id) VALUES('2', 'Renovar DNI', '1');
+INSERT INTO tareas (id, titulo, usuario_id) VALUES('3', 'Comprar pan', '1');
